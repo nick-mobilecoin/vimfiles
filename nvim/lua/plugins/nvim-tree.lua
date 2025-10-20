8e97629a204a6d7cd19ce6659dcad952c705d564
Nick Santana <nick@mobilecoin.com>
2025-10-20 08:51:38 -0700

    GIT :/
    Have nvim tree kind of working

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,1 +1,11 @@
-return { "nvim-tree/nvim-tree.lua", name = "nvim-tree" }
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+}
